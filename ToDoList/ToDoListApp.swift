cf5b3b880489d5581861e52fd247830e7ff262c4
Aimee Hong <honglp@bc.edu>
2024-10-15 19:45:21 -0400

    created function to purge preview data

--- ToDoList/ToDoListApp.swift
+++ ToDoList/ToDoListApp.swift
@@ -20,5 +20,16 @@ struct ToDoListApp: App {
     // Will allow us to find where our simulator data is saved:
     init() {
         print(URL.applicationSupportDirectory.path(percentEncoded: false))
+//        purgeData()
+    }
+    
+    func purgeData() {
+        let path = URL.documentsDirectory.appending(components: "toDos")
+        let data = try? JSONEncoder().encode("")
+        do {
+            try data?.write(to: path)
+        } catch {
+            print("😡 ERROR: Could not save data \(error.localizedDescription)")
+        }
     }
 }
